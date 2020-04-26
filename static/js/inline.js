6aa80b4bb7c78b20f91fe29854ecb899345dec5a
Sam Giles <sam.giles@citymapper.com>
2020-04-26 20:39:41 +0100

    Update inline loading

---
+++ static/js/inline.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', function(ev) {
+  function loadsrcs(srcs) {
+      var i = srcs.length;
+      var frag = document.createDocumentFragment();
+      while(i--) {
+        var s = document.createElement('script');
+        var e = srcs[i];
+        if (e[1]) {
+          s.async = s.defer = true;
+        }
+        s.src = e[0];
+        frag.appendChild(s);
+      }
+      var sel = document.getElementsByTagName('script')[0];
+      sel.parentNode.insertBefore(frag, sel);
+  }
+  var srcs = [];
+  if ('ArrayBuffer' in window && 'DataView' in window && 'Uint8Array' in window && 'Blob' in window && 'sendBeacon' in navigator) {
+    window.___pageLoadTime = Date.now();
+    srcs.push(['/js/extra.min.js', true]);
+  }
+  
+  if (!('Promise' in window)) {
+    srcs.push(["https://polyfill.io/v3/polyfill.min.js?features=es6", false]);
+  }
+
+  if (srcs.length > 0) {
+    loadsrcs(srcs);
+  }
+});
+
